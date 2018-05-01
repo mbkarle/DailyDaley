5f9a66685593c5c9bcd5db8260f5bdd97c76bf7c
Matt Karle <mkarle2178@gmail.com>
2018-05-01 12:31:14 -0400

    updated background to enable on install

--- background.js
+++ background.js
@@ -1,10 +1,13 @@
 //background js
-chrome.declarativeContent.onPageChanged.removeRules(undefined,function(){
-    chrome.declarativeContent.onPageChanged.addRules([{
-        conditions: [new chrome.declarativeContent.PageStateMatcher({
-            pageUrl: {hostEquals: '*'},
-        })
-        ],
-        actions: [new chrome.declarativeContent.ShowPageAction()]
-    }]);
+chrome.runtime.onInstalled.addListener(function(){
+    chrome.storage.sync.set({DailyDaley: true}, function(){console.log("Enabled");});
+    chrome.declarativeContent.onPageChanged.removeRules(undefined,function(){
+        chrome.declarativeContent.onPageChanged.addRules([{
+            conditions: [new chrome.declarativeContent.PageStateMatcher({
+                pageUrl: {hostEquals: '*'},
+            })
+            ],
+            actions: [new chrome.declarativeContent.ShowPageAction()]
+        }]);
+    });
 });
